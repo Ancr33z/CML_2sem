b770d296ba76e6ae08cc2819e4850124c6395710
Ancr33z <nikita.borisov20011@gmail.com>
2024-04-27 21:25:12 +0300

    .

--- js/scriptForCalc.js
+++ js/scriptForCalc.js
@@ -100,10 +100,10 @@ window.addEventListener('DOMContentLoaded', () => {
         thanksModal.classList.add('modal__dialog');
         thanksModal.innerHTML = `
             <div class="modal__content">
-                <div class="modal__close" data-close>×</div>
                 <div class="modal__title">${message}</div>
             </div>
         `;
+
         document.querySelector('.modal').append(thanksModal);
         setTimeout(() => {
             thanksModal.remove();
